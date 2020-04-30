2bb0135b07261e02287a1df70edaaffa4a6e98da
Melanie Hallowell <melleehall@gmail.com>
2020-04-29 23:32:11 -0600

    Wrote notes routers to perform CRUD operations

---
+++ src/notes/notes-router.js
@@ -0,0 +1,107 @@
+const express = require('express')
+const xss = require('xss')
+const NotesService = require('./notes-service')
+
+const notesRouter = express.Router()
+const jsonParser = express.json()
+
+const serializeNotes = note => ({
+    id: note.id,
+    note_name: xss(note.note_name),
+    modified_date: note.modified_date,
+    content: xss(note.content),
+    folder_id: note.folder_id
+})
+
+notesRouter
+    .route('/')
+    .get((req, res, next) => {
+        NotesService.getAllNotes(
+            req.app.get('db')
+        )
+        .then(notes => {
+            res.json(notes.map(serializeNotes))
+        })
+        .catch(next)
+    })
+    .post(jsonParser, (req, res, next) => {
+        const { note_name, content, folder_id } = req.body
+        const newNote = { note_name, content, folder_id }
+
+        for (const [key, value] of Object.entries(newNote)) {
+            if (value == null) {
+                return res.status(400).json({
+                    error: { message: `Missing '${key}' in request body`}
+                })
+            }
+        }
+
+        NotesService.insertNote(
+            req.app.get('db'),
+            newNote
+        )
+
+            .then(note => {
+                res 
+                    .status(201)
+                    .location(`/api/notes/${note.id}`)
+                    .json(serializeNotes(note))
+            })
+            .catch(next)
+    })
+
+notesRouter
+    .route('/:note_id')
+    .all((req, res, next) => {
+        NotesService.getById(
+            req.app.get('db'),
+            req.params.note_id
+        )
+            .then(note => {
+                if (!note) {
+                    return res.status(404).json({
+                        error: { message: `Note doesn't exist`}
+                    })
+                }
+                res.note = note
+                next()
+            })
+            .catch(next)
+    })
+    .get((req, res, next) => {
+        res.json(serializeNotes(res.note))
+    })
+    .delete((req, res, next) => {
+        NotesService.deleteNote(
+            req.app.get('db'),
+            req.params.note_id
+        )
+            .then(numRowsAffected => {
+                res.status(204).end()
+            })
+            .catch(next)
+    })
+    .patch(jsonParser, (req, res, next) => {
+        const { note_name, content, folder_id } = req.body
+        const noteToUpdate = { note_name, content, folder_id }
+
+        const numberOfValues = Object.values(noteToUpdate).filter(Boolean).length
+        if (numberOfValues === 0)
+            return res.status(400).json({
+                error: {
+                    message: `Request body must contain either 'note_name', 'content', or 'folder_id'.`
+                }
+            })
+
+        NotesService.updateNote(
+            req.app.get('db'),
+            req.params.note_id,
+            noteToUpdate
+        )
+            .then(numRowsAffected => {
+                res.status(204).end()
+            })
+            .catch(next)
+    })
+
+module.exports = notesRouter
